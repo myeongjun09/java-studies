a3db1ced37834ee037cfacc4f157e9b87a924172
myeongjun <s2554@e-mirim.hs.kr>
2025-07-02 14:31:59 +0900

    [level 0] Title: 7의 개수, Time: 0.06 ms, Memory: 76.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        int num = 0;
+        for(int i = 0; i<array.length; i++) {
+            num = array[i];
+            while(num>0) {
+                if(num%10==7) {
+                    answer ++;
+                }
+                num /= 10;
+            }
+        }
+        return answer;
+    }
+}
