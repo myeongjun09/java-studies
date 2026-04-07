cf045ff01d60a6afcbcf34b2824d97b3f35a24cc
myeongjun <s2554@e-mirim.hs.kr>
2026-04-08 08:16:27 +0900

    [level 0] Title: 뒤에서 5등 위로, Time: 1.25 ms, Memory: 88.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181852. 뒤에서 5등 위로/뒤에서 5등 위로.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        
+        int[] answer = new int[num_list.length -5];
+        
+        int index = 0;
+        for(int i = 5; i<num_list.length; i++) {
+            answer[index] = num_list[i];
+            index++;
+        }
+        return answer;
+    }
+}
