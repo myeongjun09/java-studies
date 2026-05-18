f272a1ca2104bac708097f98265dfc324a1d0b5a
myeongjun <ryen090910@gmail.com>
2026-05-18 09:03:41 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 0.12 ms, Memory: 62.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int solution(String myString, String pat) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < myString.length(); i++) {
+            char c = myString.charAt(i);
+            if (c == 'A') {
+                sb.append('B');
+            } else if (c == 'B') {
+                sb.append('A');
+            } else {
+                sb.append(c);
+            }
+        }
+        
+        String newString = sb.toString();
+        return newString.contains(pat) ? 1 : 0;
+    }
+}
