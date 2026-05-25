f1812b9c9249582f2fbd6fe7e4284ad152e2deb2
myeongjun <ryen090910@gmail.com>
2026-05-26 08:28:51 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 0.21 ms, Memory: 61.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -0,0 +1,14 @@
+import java.util.StringTokenizer;
+class Solution {
+    public String[] solution(String my_string) {
+        StringTokenizer st = new StringTokenizer(my_string," ");
+        String [] answer = new String[st.countTokens()];
+
+        int i = 0;
+        while(st.hasMoreTokens()) {
+            answer[i] = st.nextToken();
+            i++;
+        }
+        return answer;
+    }
+}
